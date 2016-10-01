4f10af0625db264da27700c7326f43b54bceae15
Emilio Tirado <emilio.tirado57@gmail.com>
2016-10-01 11:18:00 -0400

    adding basic form for course section

---
+++ src/components/courses/courseForm.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var React = require('react');
+var Input = require('../common/textInput');
+
+var CourseForm = React.createClass({
+  propTypes: {
+    course: React.PropTypes.object.isRequired,
+    onSave: React.PropTypes.func.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+    errors: React.PropTypes.object
+  },
+
+  render: function(){
+    return (
+      <form>
+        <h1>Manage Course</h1>
+        <Input
+        name="title"
+        label="Title"
+        value={this.props.course.title}
+        onChange={this.props.onChange}
+        error={this.props.errors.title}/>
+        <Input
+        name="author"
+        label="Author"
+        value={this.props.course.author}
+        onChange={this.props.onChange}
+        error={this.props.errors.author}/>
+        <Input
+        name="length"
+        label="Length"
+        value={this.props.course.length}
+        onChange={this.props.onChange}
+        error={this.props.errors.length}/>
+        <Input
+        name="category"
+        label="Category"
+        value={this.props.course.category}
+        onChange={this.props.onChange}
+        error={this.props.errors.category}/>
+        <Input
+        name="url"
+        label="URL"
+        value={this.props.course.url}
+        onChange={this.props.onChange}
+        error={this.props.errors.url}/>
+
+        <input
+        type="submit"
+        value="Save"
+        className="btn btn-primary"
+        onClick={this.props.onSave} />
+      </form>
+    );
+  }
+});
+
+module.exports = CourseForm;
--- src/components/courses/manageCoursePage.js
+++ src/components/courses/manageCoursePage.js
@@ -1,11 +1,47 @@
 'use strict';
 
 var React = require('react');
+var CourseForm = require('./courseForm');
 
 var ManageCoursePage = React.createClass({
+  getInitialState: function(){
+    return {
+      course: { id: '', title: '', author: '', length: '', category: '', url: '' },
+      errors: {},
+      dirty: false
+    };
+  },
+
+  courseFormIsValid: function(){
+    var formIsValid = true;
+    this.state.errors = {};
+    // TODO: ADD SOME VALIDATION HERE
+
+    this.setState({errors: this.state.errors});
+    return formIsValid;
+  },
+
+  saveCourse: function(event){
+
+  },
+
+  setCourseState: function(event){
+    this.setState({dirty: true});
+
+    var field = event.target.name;
+    var value = event.target.value;
+    this.state.course[field] = value;
+
+    return this.setState({course: this.state.course});
+  },
+
   render: function(){
     return (
-      <h1>Hello World</h1>
+      <CourseForm
+        course={this.state.course}
+        onChange={this.setCourseState}
+        onSave={this.saveCourse}
+        errors={this.state.errors} />
     );
   }
 });
